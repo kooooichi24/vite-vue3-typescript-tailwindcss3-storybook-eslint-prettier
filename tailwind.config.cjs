38b8d01e68dbfb6dfd639837a5fa7973eeeef266
kooooichi24 <ex.kouichi1224@gmail.com>
2022-11-14 16:07:30 +0900

    chore: primaryカラーなどを指定できるように、tailwindの設定を拡張

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // brand colors
+        brand: "#2B62A3",
+        // custom colors
+        primary: {
+          light: "#328FE9",
+          DEFAULT: "#0065C7",
+          dark: "#00388B",
+        },
+        secondary: {
+          light: "#9299AC",
+          DEFAULT: "#4D566E",
+          dark: "#262F4A",
+        },
+        attention: {
+          light: "#E44C4C",
+          DEFAULT: "#C50707",
+          dark: "#9C0303",
+        },
+        text: {
+          primary: "#141C33",
+          secondary: "#4D566E",
+          placeholder: "#9299AC",
+          disabled: "#B5BACA",
+          contrast: "#A6ACBE",
+        },
+      },
+    },
   },
   plugins: [],
 };
